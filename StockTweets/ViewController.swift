c6a7f035a761663a852a279ed73ea14dc6043824
nikdmello <nik.dmello@gmail.com>
2019-09-11 12:20:14 -0400

    Initial Commit

---
+++ StockTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockTweets
+//
+//  Created by Nikhil D'Mello on 9/11/19.
+//  Copyright © 2019 Nikhil D'Mello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
